499a216f3968d867f2290be42df2e3eb63e70eea
amirhamzah310 <amirhamzah310@gmail.com>
2020-08-05 16:30:19 +0700

    MultiAuth

---
+++ README.md
@@ -0,0 +1,31 @@
+## Laravel 7 multiple authentication system
+
+## Clone this repo
+```
+https://github.com/amirhamzah310/Laravel7-MultiAuth.git
+```
+
+## Install composer packages
+```
+$ cd Laravel7-MultiAuth
+$ composer install
+```
+
+## Create and setup .env file
+```
+make a copy of .env.example and rename to .env
+$ php artisan key:generate
+put database credentials in .env file
+```
+
+## Migrate and insert records
+```
+$ php artisan migrate
+```
+
+## Mail setup 
+```
+visit at : https://mailtrap.io/
+put mail credentials in .env file
+```
+
---
+++ app/Http/Controllers/SuratMasukController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SuratMasuk;
+use Illuminate\Http\Request;
+
+class SuratMasukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SuratMasuk  $suratMasuk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SuratMasuk $suratMasuk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SuratMasuk  $suratMasuk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SuratMasuk $suratMasuk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SuratMasuk  $suratMasuk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SuratMasuk $suratMasuk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SuratMasuk  $suratMasuk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SuratMasuk $suratMasuk)
+    {
+        //
+    }
+}
